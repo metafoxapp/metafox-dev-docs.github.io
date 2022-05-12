b78e9931d9d13f4673a52da8113410502f1debad
Paul Kellow <paul@phpfox.com>
2022-05-12 16:32:47 +0700

    init branch

---
+++ README.md
@@ -0,0 +1,25 @@
+# Documentation
+
+[dev-docs.metafoxapp.com](https://dev-docs.metafoxapp.com/)
+
+Checkout develop branch
+
+Do not working directly on `main` branch. Push changed to `develop` branch then github action will run build script then push to `main` branch.
+
+## Installation
+
+```bash
+yarn
+```
+
+## Start
+
+```bash
+yarn start
+```
+
+## Build
+
+```bash
+yarn build
+```
---
+++ doczrc.js
@@ -0,0 +1,36 @@
+export default {
+  port: 3004,
+  base: '/',
+  typescript: true,
+  title: 'MetaFox',
+  description: 'MetaFox',
+  src: './src',
+  menu: [
+    { name: 'Getting Started' },
+    {
+      name: 'Backend',
+      menu: [
+        { name: 'Installation' },
+        { name: 'Structure' },
+        { name: 'Package' },
+        { name: 'Routing' },
+        { name: 'Controller' },
+        { name: 'Eloquent' },
+        { name: 'Event' },
+        { name: 'Content' },
+        {
+          name: 'Appendix',
+          menus: [{ name: 'Event List' }]
+        }
+      ]
+    },
+    {
+      name: 'Frontend',
+      menu: [{ name: 'Getting Started' }]
+    },
+    {
+      name: 'Example',
+      menu: [{ name: 'Getting Started' }]
+    }
+  ]
+};
